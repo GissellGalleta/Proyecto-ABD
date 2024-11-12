846fc148c8c8077252719c3418fa0cd93030f965
Mario-PinedaD <mariopd2003@hotmail.com>
2024-11-11 22:45:44 -0600

    Usuario para bitacora 'auditor'

---
+++ usuarios.sql
@@ -0,0 +1,31 @@
+-- Esta es una lista con los comandos para la gestión de usuarios en mysql y postgres:
+-- ==================== POSTGRESQL
+-- AUTIDOR
+-- Todo esto es para poder darle únicamente permisos de consulta al usuario administrador
+CREATE USER auditor WITH PASSWORD 'auditor';
+REVOKE ALL ON SCHEMA registros_bitacora FROM auditor;
+REVOKE ALL ON ALL TABLES IN SCHEMA registros_bitacora FROM auditor;
+GRANT USAGE ON SCHEMA registros_bitacora TO auditor; -- Conceder acceso al esquema
+GRANT SELECT ON registros_bitacora.Bitacora TO auditor; -- Conceder permisos de solo lectura a la tabla
+REVOKE INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA registros_bitacora FROM auditor;
+-- si después se quieren revocar todos los permisos es:
+REVOKE SELECT ON registros_bitacora.Bitacora FROM auditor;
+
+-- revocar el acceso al otro esquema
+REVOKE USAGE ON SCHEMA contabilidad FROM auditor;
+-- eliminar permisos de todo el esquema:
+REVOKE ALL ON ALL TABLES IN SCHEMA contabilidad FROM auditor;
+
+
+
+
+-- ============= MYSQL
+CREATE USER 'auditor'@'localhost' IDENTIFIED BY 'auditor';
+GRANT SELECT ON nombre_base_datos.nombre_tabla TO 'auditor'@'localhost';
+GRANT SELECT ON registros_bitacora.Bitacora TO 'auditor'@'localhost';
+REVOKE ALL PRIVILEGES ON *.* FROM 'auditor'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'auditor'@'localhost';
+
+
+
