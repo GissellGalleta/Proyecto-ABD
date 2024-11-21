9900da3047914cee23dfb1232f0bc5acd979dc30
MauricioTR1 <edwiinroomero@gmail.com>
2024-11-20 21:05:55 -0600

    Consulta de una póliza (select)

--- SelectMySQL.sql
+++ SelectMySQL.sql
@@ -16,24 +16,55 @@ ORDER BY
     END, 
     CAST(C_numSubCta AS UNSIGNED);
 
--- Consulta de una póliza por año, mes y tipo, falta de darle formato y que concuerde con el documento
+-- Consulta de una póliza por año, mes, tipo y folio
+SELECT * FROM (
+    SELECT
+        M.M_C_numCta AS numero_cuenta,
+        M.M_C_numSubCta AS numero_subcuenta,
+        C.C_nomSubCta AS concepto_subcuenta,
+
+        CASE 
+            WHEN M.M_monto >= 0 THEN M.M_monto
+            ELSE 0
+        END AS debe,
+
+        CASE 
+            WHEN M.M_monto < 0 THEN -M.M_monto
+            ELSE 0
+        END AS haber
+
+    FROM 
+        Polizas AS P
+    JOIN 
+        Movimientos AS M ON P.P_anio = M.M_P_anio 
+                         AND P.P_mes = M.M_P_mes 
+                         AND P.P_dia = M.M_P_dia 
+                         AND P.P_tipo = M.M_P_tipo 
+                         AND P.P_folio = M.M_P_folio
+    JOIN 
+        Cuentas AS C ON M.M_C_numCta = C.C_numCta 
+                     AND M.M_C_numSubCta = C.C_numSubCta
+    WHERE 
+        P.P_anio = 2023
+        AND P.P_mes = 12
+        AND P.P_tipo = 'E'
+        AND P.P_folio = 9
+    ORDER BY 
+        M.M_numMov
+) AS consulta1
+UNION
 SELECT 
-    CONCAT(P.P_anio, '-', LPAD(P.P_mes, 2, '0'), '-', LPAD(P.P_dia, 2, '0')) AS fecha, -- Fecha en formato YYYY-MM-DD
-    M.M_C_numCta AS numero_cuenta,
-    M.M_C_numSubCta AS numero_subcuenta,
-    C.C_nomSubCta AS concepto_subcuenta,
-
-    -- Determinar si el monto está en debe o haber
-    CASE 
-        WHEN M.M_monto >= 0 THEN M.M_monto -- Montos positivos van al debe
-        ELSE 0                            -- Montos negativos no van al debe
-    END AS debe,
-
-    CASE 
-        WHEN M.M_monto < 0 THEN -M.M_monto -- Montos negativos van al haber
-        ELSE 0                            -- Montos positivos no van al haber
-    END AS haber
-
+    '' AS numero_cuenta,
+    '' AS numero_subcuenta,
+    'Total' AS concepto_subcuenta,
+    SUM(CASE 
+            WHEN M.M_monto >= 0 THEN M.M_monto
+            ELSE 0
+        END) AS debe,
+    SUM(CASE 
+            WHEN M.M_monto < 0 THEN -M.M_monto
+            ELSE 0
+        END) AS haber
 FROM 
     Polizas AS P
 JOIN 
@@ -42,17 +73,40 @@ JOIN
                      AND P.P_dia = M.M_P_dia 
                      AND P.P_tipo = M.M_P_tipo 
                      AND P.P_folio = M.M_P_folio
-JOIN 
-    Cuentas AS C ON M.M_C_numCta = C.C_numCta 
-                 AND M.M_C_numSubCta = C.C_numSubCta
-
 WHERE 
     P.P_anio = 2023
     AND P.P_mes = 12
-    AND P.P_tipo = 'I'
+    AND P.P_tipo = 'E'
+    AND P.P_folio = 9
+UNION
+SELECT
+    "Fecha" AS numero_cuenta,
+    "Folio" AS numero_subcuenta,
+    "Hecho Por" AS concepto_subcuenta,
+    "Revisado por" AS debe,
+    "Autorizado por" AS haber
+FROM 
+    Polizas AS P
+WHERE 
+    P.P_anio = 2023
+    AND P.P_mes = 12
+    AND P.P_tipo = 'E'
+    AND P.P_folio = 9
+UNION
+    SELECT
+        DISTINCT CONCAT(P.P_anio, '-', LPAD(P.P_mes, 2, '0'), '-', LPAD(P.P_dia, 2, '0')) AS numero_cuenta,
+        P.P_folio AS numero_subcuenta,
+        P.P_hechoPor AS concepto_subcuenta,
+        P.P_revisadoPor AS debe,
+        P.P_autorizadoPor AS haber
+    FROM 
+        Polizas AS P
+    WHERE 
+        P.P_anio = 2023
+        AND P.P_mes = 12
+        AND P.P_tipo = 'E'
+        AND P.P_folio = 9;
 
-ORDER BY 
-    fecha, M.M_numMov;
 
 
 
